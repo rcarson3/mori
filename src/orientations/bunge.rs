c7938976d9007d127b7e3105efc97cf44b61a0b7
rcarson3 <rac428@cornell.edu>
2018-03-05 16:36:13 -0500

    Small bug fix and Apache-2 copy right added
    Fixed a small bug where when the self conversion of a orientation was
    called it would consume itself in the process while producing a clone
    operation.
    
    Also added the Apache-2 copyright from the License file to the top of
    each file.

--- src/orientations/bunge.rs
+++ src/orientations/bunge.rs
@@ -1,3 +1,17 @@
+// This file is a part of the mori - Material Orientation Library in Rust
+// Copyright 2018 Robert Carson
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+// limitations under the License.
 
 use super::*;
 
@@ -40,7 +54,7 @@ impl Bunge{
     }//End of new_init
 
     //Just returns self if called
-    pub fn to_bunge(self) -> Bunge{
+    pub fn to_bunge(&self) -> Bunge{
         self.clone()
     }//End of to_bunge
 
